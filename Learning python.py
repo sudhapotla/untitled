5877fbaa2864e8d027bea85aaa29efec59c1923e
sudha <sudhapotla@hotmail.com>
2020-07-20 08:41:37 -0400

    worked on Format method

---
+++ Learning python.py
@@ -0,0 +1,70 @@
+print("hello world! ")
+price = 100
+price = 200
+print(price)
+print("@" * 10)
+# printing the data type
+price =20
+rate  =20.1
+name   ="sudha"
+is_name =True
+print(price)
+print(float(rate))
+print(name)
+print(is_name)
+#learning about input function
+"""name =input("what is your name? ")
+print('Hi' + name)
+name =input('what is your name')
+color=input('what is your favorite color')
+print(name  +" my color is " + color)
+birth_year = input('Birthyear : ' )
+print(type(birth_year))
+age =2019-int(birth_year)
+print(type(age))
+print(age)"""
+"""weight_lbs=input("what is your weight in pounds: ")
+weight_kgs= int(weight_lbs )* 0.45
+print(weight_kgs)"""
+#using format method
+first = 'sudha'
+last =  'potla'
+message= 'first +"[' + last + ']" is a goodgirl'
+msg=f'{first}[{last}] is a goodgirl'
+print(msg)
+#using the length function
+character= "sudha is a very good girl"
+print ("Sudha" in character)
+#print(len(character))
+#print(character.replace('good girl', 'apsolutely good girl'))
+#learning if condition
+is_hot= True
+if is_hot:
+    #print("it's a cold day")
+    print("wear warm clothes")
+    print("enjoy your day")
+name= 'sudha'
+#Format eg
+course="(python)"
+week=5
+participant=3
+message=("all {} participants meet {} times a week and practice")
+print("all {} participants meet {} times a week and practice {} everyday")
+print(message.format(participant,week,course) + " everyday")
+name='Sudha'
+print(name.index("d"))
+print("the location of d in sudha is",name.index("d"))
+#print(name.index("k"))
+#print("the location of k in sudha is",name.index("k"))
+#Operator	Name	Example	Try it
+"""+	Addition	x + y
+-	Subtraction	x - y
+*	Multiplication	x * y
+/	Division	x / y
+%	Modulus	x % y
+**	Exponentiation	x ** y
+//	Floor division	x // y"""
+
+
+
+
